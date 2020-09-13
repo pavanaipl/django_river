9773faba721221a9985f75c70c5827146f9fb8ba
pavanaipl <pavan@appinessworld.com>
2020-09-13 23:50:12 +0530

    initial commit

---
+++ demo/models.py
@@ -0,0 +1,6 @@
+# from django.db import models
+#
+# # Create your models here.
+# from river.models.fields.state import StateField
+# class MyModel(models.Model):
+#     my_state_field = StateField()
